e6b85c494cf873003dcbaea38987130c7d1ab652
Joanis Rouanet <joanis.ang3@gmail.com>
2023-05-27 13:42:54 +0200

    Add tests folder

---
+++ tests/BundleTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of ang3/money-bundle.
+ */
+
+namespace Ang3\Bundle\MoneyBundle\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
+final class BundleTest extends TestCase
+{
+    public function testDefault(): void
+    {
+        static::assertTrue(true);
+    }
+}
