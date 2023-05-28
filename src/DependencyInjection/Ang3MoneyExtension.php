b6aac649a739999fc005a25da3fc0383b7fb9e51
Joanis Rouanet <joanis.ang3@gmail.com>
2023-05-28 15:04:50 +0200

    Remove configuration validation due to currency registry creation

--- src/DependencyInjection/Ang3MoneyExtension.php
+++ src/DependencyInjection/Ang3MoneyExtension.php
@@ -11,13 +11,11 @@ declare(strict_types=1);
 
 namespace Ang3\Bundle\MoneyBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\Intl\Currencies;
 
 class Ang3MoneyExtension extends Extension implements PrependExtensionInterface
 {
@@ -25,7 +23,6 @@ class Ang3MoneyExtension extends Extension implements PrependExtensionInterface
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-        $this->validateConfiguration($config);
         $container->setParameter('ang3_money.config', $config);
 
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
@@ -49,22 +46,6 @@ class Ang3MoneyExtension extends Extension implements PrependExtensionInterface
                     ],
                 ],
             ]);
-        }
-    }
-
-    private function validateConfiguration(array $config): void
-    {
-        $isoCurrencyCodes = ($config['iso_currencies']['enabled'] ?? false) ? (($config['iso_currencies']['codes'] ?? []) ?: Currencies::getCurrencyCodes()) : [];
-        $customCurrencyCodes = array_reduce($config['custom_currencies'] ?? [], function ($result, $parameters) {
-            $result[] = $parameters['code'];
-
-            return $result;
-        }, []);
-
-        $allCurrencyCodes = array_merge($isoCurrencyCodes, $customCurrencyCodes);
-
-        if (!\in_array($config['default_currency'], $allCurrencyCodes, true)) {
-            throw new InvalidConfigurationException(sprintf('The default currency "%s" is not valid - In case of ISO currency, make sure ISO currencies are enabled and the currency is not filtered, otherwise do not forget to register the custom currency.', $config['default_currency']));
         }
     }
 }
